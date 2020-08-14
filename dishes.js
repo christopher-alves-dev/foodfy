fca084a8931b271658eb8b376a32ea42db22eddf
chrisleo-usa <chrisleo.usa@gmail.com>
2020-08-14 15:18:13 -0300

    adjusted data.json

--- dishes.js
+++ dishes.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const data = require("./data.json");
 
 exports.about = function(req, res) {
@@ -39,7 +41,40 @@ exports.index = function(req, res) {
 
 exports.create = function(req, res ) {
   //TODO: PÁGINA CREATE. 
-  return res.render("admin/create", { recipes: data.recipes })
+  return res.render("admin/create")
+}
+
+exports.post = function(req, res ) {
+
+  const keys = Object.keys(req.body);
+
+  for(key of keys) {
+    if(req.body[key] == '') {
+      return res.send('Fill all the fields plz!');
+    }
+  }
+
+  let { image, title, name, ingredients, preparation, textArea } = req.body
+
+  const id = Number(data.recipes.length + 1);
+
+  data.recipes.push({
+    id,
+    image,
+    title,
+    name,
+    ingredients, 
+    preparation, 
+    textArea
+  })
+
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+    if(err) return res.send('Write file error!');
+    
+    return res.redirect('/admin/recipes');
+  })
+
+  // return res.send(req.body)
 }
 
 exports.showAdmin = function(req, res ) {
@@ -63,13 +98,22 @@ exports.showAdmin = function(req, res ) {
 }
 
 exports.edit = function(req, res ) {
-  
+  const { id } = req.params;
 
-  return res.render("admin/edit", { recipes: data.recipes })
-}
+  const foundRecipe = data.recipes.find(function(recipe) {
+    return recipe.id == id
+    
+  })
 
-exports.post = function(req, res ) {
-  
+  if(!foundRecipe) {
+    return res.render("not-found");
+  }
+
+  const recipe = {
+    ...foundRecipe
+  }
+
+  return res.render("admin/edit", { recipe })
 }
 
 exports.put = function(req, res ) {
